969f68dcc481a599c9c1d213195c7b83c2f2c99b
vitoraa <vitor.lima@outlook.com>
2021-09-14 11:53:17 -0300

    use better solution

--- general/has-pair-with-sum.js
+++ general/has-pair-with-sum.js
@@ -1,4 +1,4 @@
-const hasPairWithSum = (sum, array) => {
+/* const hasPairWithSum = (sum, array) => {
   for (let i = 0; i < array.length - 1; i++) {
     for (let j = i + 1; j < array.length; j++) {
       if (sum === array[i] + array[j]) return true
@@ -6,6 +6,20 @@ const hasPairWithSum = (sum, array) => {
   }
 
   return false;
+} */
+
+const hasPairWithSum = (sum, array) => {
+  let allNumbers = new Set()
+
+  for (let i = 0; i < array.length; i++) {
+    if (allNumbers.has(array[i])) {
+      return true
+    }
+    allNumbers.add(sum - array[i])
+  }
+
+  return false;
 }
 
-console.log(hasPairWithSum(10, [1, 4, 5, 8, 7]))
+
+console.log(hasPairWithSum(15, [1, 4, 5, 8, 9]))

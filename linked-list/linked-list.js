28688a1b92a9cdf2d86eb900ba2b2f3a025dbd49
vitoraa <vitor.lima@outlook.com>
2021-09-16 17:11:56 -0300

    append function created

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -7,7 +7,21 @@ class LinkedList {
     this.tail = this.head
     this.length = 1
   }
+
+  append(value) {
+    const newNode = {
+      value,
+      next: null
+    }
+    this.tail.next = newNode
+    this.tail = newNode
+    this.length++
+  }
 }
 
 let myLinkedList = new LinkedList(10)
+myLinkedList.append(11)
+myLinkedList.append(12)
+myLinkedList.append(13)
+myLinkedList.append(14)
 console.log(myLinkedList)

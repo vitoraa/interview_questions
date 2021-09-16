0a8555359cfa0cf15226a6a5c32f71c1ba62ac0c
vitoraa <vitor.lima@outlook.com>
2021-09-16 17:19:22 -0300

    Node class

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -1,28 +1,27 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
 class LinkedList {
   constructor(value) {
-    this.head = {
-      value,
-      next: null
-    }
+    this.head = new Node(value)
     this.tail = this.head
     this.length = 1
   }
 
   append(value) {
-    const newNode = {
-      value,
-      next: null
-    }
+    const newNode = new Node(value)
     this.tail.next = newNode
     this.tail = newNode
     this.length++
   }
 
   prepend(value) {
-    const newNode = {
-      value,
-      next: this.head
-    }
+    const newNode = new Node(value)
+    newNode.next = this.head
     this.head = newNode
     this.length++
   }

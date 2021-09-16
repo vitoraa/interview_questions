2838e48b302c23a472fe11d2fb42ec6626850277
vitoraa <vitor.lima@outlook.com>
2021-09-16 15:07:08 -0300

    some improvements

--- hash/hash.js
+++ hash/hash.js
@@ -6,20 +6,34 @@ class HashTable {
   _hash(key) {
     let hash = 0
     for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * i) %
-        this.data.length
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length
     }
     return hash
   }
 
   set(key, value) {
     let hashKey = this._hash(key)
-    this.data[hashKey] = value
+    if (!this.data[hashKey]) {
+      this.data[hashKey] = []
+    } else {
+      for (let item of this.data[hashKey]) {
+        if (item[0] === key) {
+          item[1] = value
+          return
+        }
+      }
+    }
+    this.data[hashKey].push([key, value])
   }
 
   get(key) {
     let hashKey = this._hash(key)
-    return this.data[hashKey]
+    for (let item of this.data[hashKey]) {
+      if (item[0] === key) {
+        return item[1]
+      }
+    }
+    return undefined
   }
 }
 
@@ -27,5 +41,6 @@ const myHashTable = new HashTable(50)
 myHashTable.set('grapes', 1000)
 myHashTable.set('banana', 2000)
 myHashTable.set('mellon', 3000)
-let a = myHashTable.get('grapes')
+myHashTable.set('mellon', 40000)
+let a = myHashTable.get('mellon')
 console.log(myHashTable, a)

f1503d69b5a3ab7a1917bfef25d0e6d56cc3012f
vitoraa <vitor.lima@outlook.com>
2021-09-26 15:26:38 -0300

    insert method

--- tree/binary-tree/binary-tree.js
+++ tree/binary-tree/binary-tree.js
@@ -12,13 +12,36 @@ class BinarySearchTree {
   }
 
   insert(value) {
+    const newNode = new Node(value)
+
     if (this.root === null) {
-      const newNode = new Node(value)
       this.root = newNode
       return this.root
     }
 
+    let nodeAux = this.root
+    let nodeToInsert = null
+    let nodePreAux = null
 
+    while (!nodeToInsert) {
+      if (nodeAux === null) {
+        nodeToInsert = nodePreAux
+      }
+
+      nodePreAux = nodeAux
+
+      if (value >= nodeAux?.value) {
+        nodeAux = nodeAux.rigth
+      } else if (value < nodeAux?.value) {
+        nodeAux = nodeAux.left
+      }
+    }
+
+    if (value >= nodeToInsert.value) {
+      nodeToInsert.rigth = newNode
+    } else {
+      nodeToInsert.left = newNode
+    }
   }
 
   lookup(value) {
@@ -33,9 +56,9 @@ tree.insert(6)
 tree.insert(20)
 tree.insert(170)
 tree.insert(15)
-tree.insert(11)
+tree.insert(1)
 
-JSON.stringify(traverse(tree.root))
+console.log(JSON.stringify(traverse(tree.root)))
 
 function traverse(node) {
   const treeTraverse = { value: node.value }

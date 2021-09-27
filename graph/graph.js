de5399264829c625b7dce959dbf87369f0294007
vitoraa <vitor.lima@outlook.com>
2021-09-27 16:35:11 -0300

    add edge function

--- graph/graph.js
+++ graph/graph.js
@@ -5,7 +5,10 @@ class Graph {
   }
 
   addEdge(node1, node2) {
-
+    if ((node1 in this.adjacentList) && (node2 in this.adjacentList)) {
+      this.adjacentList[node1].push(node2)
+      this.adjacentList[node2].push(node1)
+    }
   }
 
   addVertex(node) {
@@ -18,5 +21,10 @@ class Graph {
 
 const graph = new Graph()
 graph.addVertex(10)
-graph.addVertex(10)
+graph.addVertex(20)
+graph.addVertex(30)
+graph.addVertex(40)
+graph.addEdge(10, 20)
+graph.addEdge(10, 30)
+graph.addEdge(10, 40)
 console.log(graph)
